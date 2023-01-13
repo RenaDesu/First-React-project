f952a045b7b263d4a1ce015c285d38c23d5d6cdf
RenaDesu <rena-desu@mail.ru>
2023-01-13 18:46:52 +0300

    Вносит корректировки в проект

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -3,7 +3,44 @@ import './Messages.css';
 const Messages = () => {
     return (
         <main className='main-messages'>
-            Сообщения
+            <section className='messages'>
+                <ul className='messages__list'>
+                    <li className='messages__list-item'>
+                        <p className='messages__chat'>Кекс</p>
+                    </li>
+                    <li className='messages__list-item'>
+                        <p className='messages__chat'>Маффин</p>
+                    </li>
+                    <li className='messages__list-item'>
+                        <p className='messages__chat'>Булочка</p>
+                    </li>
+                    <li className='messages__list-item'>
+                        <p className='messages__chat'>Буханочка</p>
+                    </li>
+                    <li className='messages__list-item'>
+                        <p className='messages__chat'>Батончик</p>
+                    </li>
+                </ul>
+            </section>
+            <section className='chat'>
+                <ul className='chat__list'>
+                    <li className='chat__message'>
+                        <p className='chat__message-text'>Мяу мяу</p>
+                    </li>
+                    <li className='chat__message'>
+                        <p className='chat__message-text'>Мяу мяу мяу</p>
+                    </li>
+                    <li className='chat__message'>
+                        <p className='chat__message-text'>Мяу мяу мяу мяу</p>
+                    </li>
+                    <li className='chat__message'>
+                        <p className='chat__message-text'>Мяу мяу мррр</p>
+                    </li>
+                    <li className='chat__message'>
+                        <p className='chat__message-text'>Мяу!</p>
+                    </li>
+                </ul>
+            </section>
         </main>
     );
 }
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,25 +1,26 @@
+import { NavLink } from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = () => {
     return (
         <nav className='main-nav'>
             <ul className='main-nav__list'>
-                <li className='main-nav__list-item main-nav__list-item--current'>
-                    <a href='/profile'>Профиль</a>
+                <li className='main-nav__list-item'>
+                    <NavLink className='main-nav__link' to='/profile'>Профиль</NavLink>
                 </li>
                 <li className='main-nav__list-item'>
-                    <a href='/messages'>Сообщения</a>
+                    <NavLink className='main-nav__link' to='/messages'>Сообщения</NavLink>
                 </li>
                 <li className='main-nav__list-item'>
-                    <a href='/news'>Новости</a>
+                    <NavLink className='main-nav__link' to='/news'>Новости</NavLink>
                 </li>
                 <li className='main-nav__list-item'>
-                    <a href='/music'>Музыка</a>
+                    <NavLink className='main-nav__link' to='/music'>Музыка</NavLink>
                 </li>
             </ul>
             <ul className='main-nav__settings'>
                 <li className='main-nav__settings-item'>
-                    <a href='/settings'>Настройки</a>
+                    <NavLink className='main-nav__settings-link' to='/settings'>Настройки</NavLink>
                 </li>
             </ul>
         </nav>

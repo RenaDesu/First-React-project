32b51b7d4f4241f3067d29d04643af78616d07d3
RenaDesu <rena-desu@mail.ru>
2023-01-20 16:52:57 +0300

    Делает рефакторинг кода 3

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from './state/state';
-import { addPost } from './state/state';
-import { updateNewPostText } from './state/state';
-import { subscribe } from './state/state';
+import store from './state/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let renderEntireTree = (state) => {
   root.render(
   <React.StrictMode>
-    <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+    <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
   </React.StrictMode>
 );
 }
 
-renderEntireTree(state);
+renderEntireTree(store.getState());
 
-subscribe(renderEntireTree);
+store.subscribe(renderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/state/state.js
+++ src/state/state.js
@@ -1,52 +1,51 @@
-let renderEntireTree = () => {
-
-}
-
-let state = { 
-    profilePage: {
-        posts: [
-            {id: 1, message: 'О том, как я провел этот день.', likesCount: '5'},
-            {id: 2, message: 'Сон - это прекрасно.', likesCount: '10'},
-        ],
-    newPostText: ''
+let store = {
+    _state: { 
+        profilePage: {
+            posts: [
+                {id: 1, message: 'О том, как я провел этот день.', likesCount: '5'},
+                {id: 2, message: 'Сон - это прекрасно.', likesCount: '10'},
+            ],
+        newPostText: ''
+        },
+        messagesPage: {
+            messages: [
+                { id: 1, name: 'Кекс' },
+                { id: 2, name: 'Маффин' },
+                { id: 3, name: 'Булочка' },
+                { id: 4, name: 'Буханочка' },
+                { id: 5, name: 'Батончик' }
+            ],
+            chat: [
+                { id: 1, text: 'Мяу мяу' },
+                { id: 2, text: 'Мяу мяу мяу' },
+                { id: 3, text: 'Мяу мяу мяу мяу' },
+                { id: 4, text: 'Мяу мяу мррр' },
+                { id: 5, text: 'Мяу!' }
+            ]
+       }
+    }, 
+    getState () {
+    return this._state;
     },
-    messagesPage: {
-        messages: [
-            { id: 1, name: 'Кекс' },
-            { id: 2, name: 'Маффин' },
-            { id: 3, name: 'Булочка' },
-            { id: 4, name: 'Буханочка' },
-            { id: 5, name: 'Батончик' }
-        ],
-        chat: [
-            { id: 1, text: 'Мяу мяу' },
-            { id: 2, text: 'Мяу мяу мяу' },
-            { id: 3, text: 'Мяу мяу мяу мяу' },
-            { id: 4, text: 'Мяу мяу мррр' },
-            { id: 5, text: 'Мяу!' }
-        ]
-   }
+    _callSubscriber () {},
+    addPost () {
+        let newPost = {
+            id: 5, 
+            message: this._state.profilePage.newPostText,
+            likesCount: '0'
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    }
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 5, 
-        message: state.profilePage.newPostText,
-        likesCount: '0'
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    renderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    renderEntireTree = observer;
-}
-
-export default state;
+export default store;
 
